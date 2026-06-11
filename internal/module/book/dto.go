410b7f8b4070345f681b97434176337f3ceb7f75
LM16 <shanrui.zhang@leadimpacts.com>
2026-06-11 10:28:10 +0800

    recommend

--- internal/module/book/dto.go
+++ internal/module/book/dto.go
@@ -1,6 +1,7 @@
 package book
 
 import (
+	"encoding/json"
 	"strings"
 	"time"
 
@@ -114,13 +115,13 @@ type SelectionResponse struct {
 }
 
 type CreateBookNoteReq struct {
-	ID     *int64 `json:"id,omitempty"`
-	BookID int64  `json:"bookId" validate:"required"`
-	Note   string `json:"note" validate:"required"`
+	ID     *int64          `json:"id,omitempty"`
+	BookID int64           `json:"bookId" validate:"required"`
+	Note   json.RawMessage `json:"note" validate:"required"`
 }
 
 type UpdateBookNoteReq struct {
-	Note string `json:"note" validate:"required"`
+	Note json.RawMessage `json:"note" validate:"required"`
 }
 
 type TransReq struct {
--- internal/module/book/handler.go
+++ internal/module/book/handler.go
@@ -498,7 +498,7 @@ func (h *Handler) BookNotesCreate(w http.ResponseWriter, r *http.Request) (any, 
 	note := &data.BookNote{
 		UserID: userID,
 		BookID: input.BookID,
-		Note:   json.RawMessage(input.Note),
+		Note:   input.Note,
 	}
 	if input.ID != nil {
 		note.ID = *input.ID
@@ -525,7 +525,7 @@ func (h *Handler) BookNotesUpdate(w http.ResponseWriter, r *http.Request) (any, 
 	if err := httputil.DecodeAndValidate(r, &input); err != nil {
 		return nil, exception.InvalidBody
 	}
-	note, err := h.userRepo.UpdateBookNote(id, userID, json.RawMessage(input.Note))
+	note, err := h.userRepo.UpdateBookNote(id, userID, input.Note)
 	if err != nil {
 		return nil, BookNotFound.WithMsg(fmt.Sprintf("Book #%d not found", id))
 	}
